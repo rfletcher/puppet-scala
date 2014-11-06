939998363b8a749949599baf1229589e1b35d917
Rick Fletcher <fletch@pobox.com>
2014-11-06 15:53:30 -0500

    Install the scala stack using our manifests

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,12 @@
 # == Class: scala
 #
-# Install scala from the Typesafe repository
+# Install the Typesafe scala stack
 #
 class scala {
   validate_re($::osfamily, '^Debian$', "This module relies on a deb package \
 and only works on Debian based distros")
 
-  include ::scala::repository
-
-  package { [
-    'typesafe-stack',
-    'scala',
-  ]:
-    ensure  => installed,
-    require => Class['::scala::repository'],
-  }
+  include ::scala::giter8
+  include ::scala::lang
+  include ::scala::sbt
 }
